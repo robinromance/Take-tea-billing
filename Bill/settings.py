4d3480f5d613a5e30e926c8e6f2a3272727feb74
robin <robinhipro@gmail.com>
2026-01-31 17:17:28 +0530

    added static root

--- Bill/settings.py
+++ Bill/settings.py
@@ -115,12 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
+import os
 
 # STATIC
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # MEDIA (IMAGE UPLOAD)
 MEDIA_URL = '/media/'
